401e65ca2c591c1cf84b8a70d3355fffc7da80c5
HATBE2113 <admin@aarongensetter.ch>
2024-10-05 08:59:08 +0200

    swagger

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.HelloService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +18,7 @@ public class HelloController {
     }
 
     @GetMapping("/")
-    //@Operation(summary = "Returns a random greeting")
+    @Operation(summary = "Returns a random greeting")
     public ResponseEntity<String> index() {
         return ResponseEntity.ok(helloService.getGreeting());
     }
