bf0522233cf72367fe4d0fd3ee21cfa66b285f0a
HATBE <admin@aarongensetter.ch>
2024-11-21 19:16:33 +0100

    se and deserialize

--- tasks/src/main/java/ch/hatbe/juventus/NioExcercises/NioExcercises.java
+++ tasks/src/main/java/ch/hatbe/juventus/NioExcercises/NioExcercises.java
@@ -1,23 +1,28 @@
 package ch.hatbe.juventus.NioExcercises;
 
-import java.io.IOException;
+import ch.hatbe.juventus.object.Person;
+
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class NioExcercises {
     public static void main(String[] args) {
         try {
-            NioExcercises.createDirectory("/home/ag/test");
+            /*NioExcercises.createDirectory("/home/ag/test");
             NioExcercises.createFile("/home/ag/test/test.txt");
             NioExcercises.writeToFile("/home/ag/test/test.txt", "Hello World");
             NioExcercises.renameFile("/home/ag/test/test.txt", "/home/ag/test/test2.txt");
             System.out.println(NioExcercises.checkIfFileExists("/home/ag/test/test2.txt"));
             System.out.println(NioExcercises.getContentOfFile("/home/ag/test/test2.txt"));
             NioExcercises.listAllFilesFromDirectory("/home/ag/test");
-            NioExcercises.deleteContentsOfDirectory("/home/ag/test");
-        } catch (IOException e) {
+            NioExcercises.deleteContentsOfDirectory("/home/ag/test");*/
+            NioExcercises.serializePerson();
+            NioExcercises.deserializePerson();
+
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
@@ -55,9 +60,7 @@ public class NioExcercises {
 
     public static void listAllFilesFromDirectory(String path) throws IOException {
         Path directory = Paths.get(path);
-        Files.list(directory).forEach((dir -> {
-            System.out.println(dir);
-        });
+        Files.list(directory).forEach(System.out::println);
     }
 
     public static void deleteContentsOfDirectory(String path) throws IOException {
@@ -69,5 +72,19 @@ public class NioExcercises {
                 throw new RuntimeException(e);
             }
         });
+    }
+
+    public static void serializePerson() throws IOException {
+        Person person = new Person("Hans", "Muster", null, 30, false);
+        String file = "/home/ag/test/person.ser";
+        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(file));
+        out.writeObject(person);
+    }
+
+    public static void deserializePerson() throws IOException, ClassNotFoundException {
+        String file = "/home/ag/test/person.ser";
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream(file));
+        Person person = (Person) in.readObject();
+        System.out.println(person);
     }
 }
